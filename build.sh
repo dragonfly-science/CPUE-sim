c9118a05d4dc1428a1b6f6ec846335908f716a11
Philipp-Neubauer <neubauer.phil@gmail.com>
2023-03-14 14:57:49 +1300

    run on gateaux

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -exu
+Rscript CPUE_sim.r ||true
+
+if test -n "$(find . -maxdepth 1 -name '*.Rdata' -print -quit)"
+then
+    cp *.Rdata ${OUTPUTDIR}/.
+fi
+
+if test -n "$(find . -maxdepth 1 -name '*.rda' -print -quit)"
+then
+    cp *.rda ${OUTPUTDIR}/.
+fi
+
+exit $?
+if test -n "$(find . -maxdepth 1 -name '*.csv' -print -quit)"
+then
+    cp *.csv ${OUTPUTDIR}/.
+fi
